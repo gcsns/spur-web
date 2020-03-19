ad274c97aa7f8d411b10b69b4fafe9d6a36eed22
Varun Grover <varun.grover@gamechangesns.com>
2020-03-19 18:10:48 +0530

    removed reference to express-device

--- src/middleware/DefaultMiddleware.js
+++ src/middleware/DefaultMiddleware.js
@@ -8,7 +8,6 @@ module.exports = function (bodyParser, cookieParser, methodOverride, expressDevi
       this.app.use(bodyParser.urlencoded({ extended: false }));
       this.app.use(bodyParser.json());
       this.app.use(methodOverride());
-      this.app.use(expressDevice.capture());
     }
 
   }
